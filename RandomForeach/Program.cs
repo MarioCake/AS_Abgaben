9ea8c4618c551342b055e5857f5873f86de44037
David Leinweber <davidleinweber@web.de>
2018-05-10 02:29:42 +0200

    Implementierung eines Zufallszahlengenerators mit IEnumerable und IEnumerator. 2 Bespiele sind vorhanden, wie man das implementieren kann.
    Beide Varianten sind in der foreach Schleife verwendbar

---
+++ RandomForeach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RandomForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomNumberGenerator1 generator1 = new RandomNumberGenerator1(10, 10);
+            RandomNumberGenerator2 generator2 = new RandomNumberGenerator2(10, 10);
+
+            foreach (int number in generator1) Console.WriteLine(number);
+
+            Console.WriteLine("----------------------------------");
+
+            foreach (int number in generator2) Console.WriteLine(number);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RandomForeach/RandomNumberGenerator2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomForeach
+{
+    class RandomNumberGenerator2 : IEnumerable<int>
+    {
+        private int _min;
+        private int _max;
+
+        private uint _amount;
+        private Random random = new Random();
+        public RandomNumberGenerator2(uint amount, int max) : this(amount, 0, max) { }
+
+        public RandomNumberGenerator2(uint amount, int min, int max)
+        {
+            _amount = amount;
+            _min = min;
+            _max = max;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for(int i = 0; i < _amount; i++) yield return random.Next(_min, _max);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
