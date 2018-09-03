e7cbfb5a8e6b84c789d1cf2025845e0528e59cb1
David Leinweber <davidleinweber@web.de>
2018-09-03 16:37:17 +0200

    Abgabe: Methoden Erstellen 1.1

---
+++ MethodenErstellung/MethodenArrays.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodenErstellung
+{
+    public class MethodenArrays : School.SchoolTask
+    {
+        public MethodenArrays(): base("Methoden und Arrays") { }
+
+        protected override void Main(string[] args)
+        {
+            const int ANZ = 5;
+            double[] flugpreise = new double[ANZ];
+            double mittelpreis = 0.0;
+
+            flugpreise = Eingabe(ANZ);
+            mittelpreis = Durchschnittrechnung(flugpreise, ANZ);
+
+            for (int i = 0; i < ANZ; i++)
+            {
+                Console.WriteLine((i + 1) + ". Preis lautet: " + flugpreise[i] + " Euro");
+            }
+            Console.WriteLine("Durchschnittspreis:" + mittelpreis + " Euro");
+        }
+
+        public static double[] Eingabe(int anz)
+        {
+            double[] fpreise = new double[anz];
+            for (int i = 0; i < anz; i++)
+            {
+                Console.Write((i + 1) + ". Preis bitte eingeben: ");
+                fpreise[i] = double.Parse(Console.ReadLine());
+
+            }
+            return fpreise;
+        }
+
+        public static double Durchschnittrechnung(double[] preise, int anz)
+
+        {
+            double durchschnitt = 0.0;
+            for (int i = 0; i < anz; i++)
+            {
+                durchschnitt = durchschnitt + preise[i];
+            }
+            durchschnitt = durchschnitt / anz;
+            return durchschnitt;
+        }
+    }
+}
---
+++ MethodenErstellung/Preismodul.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodenErstellung
+{
+    public class Preismodul : School.SchoolTask
+    {
+        public Preismodul(): base("Preismodul") { }
+
+        // Methodendefinition
+        public static void Programmhinweis() // Methodenkopf
+        { // Methodenrumpf Beginn
+
+                 // Methodenaufruf
+            Console.WriteLine(/* Argument */"Hinweis: ");
+
+                 // Methodenaufruf
+            Console.WriteLine(/* Argument */"Das Programm addiert 2 eingegebene Zahlen. ");
+
+        } // Methodenrumpf Ende
+
+        // Methodendefinition
+        public static void Ausgabe(/* Parameter */double erg, /* Parameter */double zahl1, /* Parameter */double zahl2) // Methodenkopf
+        { // Methodenrumpf Beginn
+
+                 // Methodenaufruf
+            Console.WriteLine(/* Argument */"Ergebnis der Addition");
+
+                 // Methodenaufruf
+            Console.WriteLine(/* Argument */ erg + " = " + zahl1 + " + " + zahl2);
+
+        } // Methodenrumpf Ende
+
+        // Methodendefinition
+        public static /* Rückgabedatentyp */double Verarbeitung(/* Parameter */double zahl1, /* Parameter */ double zahl2) // Methodenkopf
+        { // Methodenrumpf Beginn
+
+            return /* Rückgabewert */ zahl1 + zahl2;
+
+        } // Methodenrumpf Ende
+
+
+        // Methodendefinition
+        public static /* Rückgabedatentyp */(double, double) Eingabe() // Methodenkopf
+        { // Methodenrumpf Beginn
+
+                 // Methodenaufruf
+            Console.WriteLine(" 1. Zahl:");
+
+                               // Methodenaufruf              Methodenaufruf
+            double zahl1 = double.Parse(/* Argument */Console.ReadLine());
+
+                 // Methodenaufruf
+            Console.WriteLine(/* Argument */" 2. Zahl:");
+
+            // Methodenaufruf
+            double zahl2 = double.Parse(/* Argument */Console.ReadLine());
+
+            return /* Rückgabewert */ (zahl1, zahl2);
+        } // Methodenrumpf Ende
+
+        protected override void Main(string[] args)
+        {
+            // Methodenaufruf
+            Programmhinweis();
+                                        // Methodenaufruf
+            (double zahl1, double zahl2) = Eingabe();
+
+                            // Methodenaufruf
+            double ergebnis = Verarbeitung(/* Argument */zahl1, /* Argument */zahl2);
+
+            // Methodenaufruf
+            Ausgabe(/* Argument */ergebnis, /* Argument */zahl1, /* Argument */zahl2);
+        }
+    }
+}
