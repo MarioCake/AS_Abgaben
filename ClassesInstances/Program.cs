fa1d82597561801b6c0ada810f79211c210771f9
David Leinweber <davidleinweber@web.de>
2018-09-07 14:00:07 +0200

    Abgabe Aufgabe Instanziierung und Klassenmethoden

---
+++ ClassesInstances/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ClassesInstances
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Raumschiff bauen:
+
+            RaumschiffUssEnterprise raumschiff = new RaumschiffUssEnterprise();
+
+
+            //Kapitän ermitteln und ausgeben:
+
+            Console.WriteLine("Kapitän: {0}", RaumschiffUssEnterprise.KapitaenErmitteln());
+
+
+            //Phaserkanonen schiessen:
+
+            raumschiff.PhaserkanoneSchiessen();
+
+
+            //Maximale Geschwindigkeit ausgeben:
+
+            Console.WriteLine("Maximalgeschwindigkeit: {0}", raumschiff.MaximalGeschwindigkeitErmitteln());
+
+
+            //Droide einsetzen:
+
+            raumschiff.DroideEinsetzen();
+
+
+            //Maximale Geschwindigkeit ausgeben:
+
+            Console.WriteLine("Maximalgeschwindigkeit: {0}", raumschiff.MaximalGeschwindigkeitErmitteln());
+
+
+            //Phaserkanone schiessen:
+
+            raumschiff.PhaserkanoneSchiessen();
+
+            Console.ReadKey();
+
+        }
+
+        /**
+         * Frage:
+         *  Stellen Sie sich vor Sie müssten noch 3 weitere 
+         *  Raumschiffe (Tie Fighter, x-Wing und die Herz aus Gold) erstellen, 
+         *  ist dieser Quellcode dafür geeignet? 
+         *  Begründen Sie Ihre Antwort.
+         * 
+         * Antwort:
+         *  Der Code ist dafür nicht geeignet. Die Klasse beschreibt eine Art
+         *  von Raumschiff und nicht ein Raumschiff im allgemeinen. Auch müssten 
+         *  die Attribute anpassbar sein selbst wenn es eine allgemeine Klasse wäre
+         *  für Raumschiffe.
+         */
+
+        /**
+         * Frage:
+         *  Recherchieren oder nennen Sie Vor- und Nachteile von statischen 
+         *  Methoden. Sollte ein Quellcode möglichst viele oder möglichst 
+         *  wenige statische Methoden beinhalten? Nennen Sie eine Ihnen bekannte 
+         *  statische Methode.
+         * 
+         * Antwort:
+         *  Statische Methoden haben den Vorteil, dass bei gleicher Eingabe
+         *  immer gleiche sachen passieren und es keinen Zustand gibt.
+         *  Nachteil ist natürlich, dass es keinen Zustand gibt und nicht
+         *  auf vorherige Ausführungen gut bezug genommen werden kann, falls keine
+         *  statische Attribute verwendet werden.
+         *  Statische Methoden sollten grundsätzlich wenige verwendet werden, da es
+         *  nicht nach den Prinzipien der Objektorientierung entspricht.
+         *  Instanzmethoden haben den Vorteil, dass sie sich auf den Zustand eines 
+         *  Objekts beziehen kann und direkt dieses Objekt verändern kann.
+         *  Dabei hat jeder Aufruf nicht unbedingt immer die gleiche Ausgabe oder
+         *  funktionalität.
+         * 
+         */
+    }
+}
---
+++ ClassesInstances/RaumschiffUssEnterprise.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesInstances
+{
+    public class RaumschiffUssEnterprise
+
+    {
+
+        private static Random _rand = new Random();
+
+        private int _photonentorpedoAnz = 20;
+
+        private int _energieversorgung = 100;
+
+        private int _zustandSchilde = 100;
+
+        private int _zustandHuelle = 100;
+
+        private int _zustandLebenserhaltungssysteme = 100;
+
+        private const bool Droide = true;
+
+        public double MaximalGeschwindigkeitErmitteln()
+
+        {
+
+            if (_energieversorgung == 100)
+
+                return 1079252848.8;
+
+            if (_energieversorgung > 50)
+
+                return 252.792;
+
+            if (_energieversorgung > 5)
+
+                return 5.08;
+
+            return 0;
+
+        }
+
+        public void PhotonentorpedoSchiessen()
+
+        {
+
+            if (_photonentorpedoAnz == 0)
+
+                Console.WriteLine("-=*Click*=-");
+
+            else
+            {
+
+                _photonentorpedoAnz--;
+
+                Console.WriteLine("Photonentorpedo abgeschossen");
+            }
+
+        }
+
+        public void PhaserkanoneSchiessen()
+
+        {
+
+            if (_energieversorgung < 15)
+
+                Console.WriteLine("-=*Click*=-");
+
+            else
+            {
+
+                _energieversorgung = _energieversorgung - 15;
+
+                Console.WriteLine("Phaserkanone abgeschossen");
+            }
+
+        }
+
+        public void DroideEinsetzen()
+
+        {
+
+            if (Droide)
+
+            {
+
+                if (_zustandLebenserhaltungssysteme < 100)
+
+                    Wartung(ref _zustandLebenserhaltungssysteme);
+
+                else if (_energieversorgung < 100)
+
+                    Wartung(ref _energieversorgung);
+
+                else if (_zustandSchilde < 100)
+
+                    Wartung(ref _zustandSchilde);
+
+                else if (_zustandHuelle < 100)
+
+                    Wartung(ref _zustandHuelle);
+
+            }
+
+        }
+
+        public static string KapitaenErmitteln()
+        {
+
+            string[] kapitaene = { "Jonathan Archer", "Robert April", "Christopher Pike",
+
+                                   "James Tiberius Kirk", "Spock", "Willard Decker",
+
+                                   "John Harriman", "Rachel Garrett", "Jean-Luc Picard",
+
+                                   "William T. Riker", "Edward Jellico" };
+
+            return kapitaene[_rand.Next(10)];
+
+        }
+
+        private static void Wartung(ref int system)
+
+        {
+
+            system = system + 50;
+
+            if (system > 100) system = 100;
+
+        }
+
+    }
+
+}
