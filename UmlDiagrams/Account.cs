3a7cf391de9da6b5ebd148a68ce2a70fe5252c58
David Leinweber <davidleinweber@web.de>
2018-11-19 15:29:21 +0100

    OOP Abgabe

---
+++ UmlDiagrams/Account.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UmlDiagrams
+{
+    public class Account
+    {
+        public string Forename { get; private set; }
+        public string LastName { get; private set; }
+        public string ZipCode { get; private set; }
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string Phone { get; private set; }
+        public string MobilePhone { get; private set; }
+        public string EmailAddress { get; private set; }
+        public string Password { get; private set; }
+        private string PasswordPlaintext { get; set; }
+
+        public Account(): this("", "") { }
+        public Account(string email, string password, string forename = null, string lastname = null, string city = null, string street = null, string zipcode = null, string phone = null, string mobilephone = null) {
+            Forename = forename;
+            LastName = lastname;
+            ZipCode = zipcode;
+            Street = street;
+            City = city;
+            Phone = phone;
+            MobilePhone = mobilephone;
+            EmailAddress = email;
+            PasswordPlaintext = password;
+        }
+        public bool CheckEmail()
+        {
+            Regex emailRegex = new Regex("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+            return emailRegex.IsMatch(EmailAddress);
+        }
+        public void EncodePassword()
+        {
+            MD5 hasher = MD5.Create();
+            byte[] hash = hasher.ComputeHash(Encoding.UTF8.GetBytes(PasswordPlaintext));
+            Password = Encoding.ASCII.GetString(hash);
+        }
+        public void ResetPassword(string oldPassword, string newPassword)
+        {
+            if(oldPassword == PasswordPlaintext)
+            {
+                PasswordPlaintext = oldPassword;
+            }
+            else
+            {
+                throw new System.Exception("Lol kennst du dein altes passwort nicht mehr? get REKT! n00b l2p!!");
+            }
+        }
+    }
+}
---
+++ UmlDiagrams/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace UmlDiagrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account("hello@world.de", "MySecurePassword123");
+            Account account2 = new Account("hans.maier@gmail.de", "password", "Hans", "Maier");
+            Account account3 = new Account("gt@", "123456", "Giesela", "Thomas", phone: "0127483943");
+
+            CheckEmail(account1);
+            CheckEmail(account2);
+            CheckEmail(account3);
+
+            account1.ResetPassword("MySecurePassword123", "MyNewPassword");
+            account2.ResetPassword("password", "MyNewPassword");
+            try { account3.ResetPassword("password", "MyNewPassword"); } catch { Console.WriteLine("Wow. Echt nicht?"); }
+
+            account1.EncodePassword();
+            Console.WriteLine("Account 1 Password Hash: {0}", account1.Password);
+            account2.EncodePassword();
+            Console.WriteLine("Account 2 Password Hash: {0}", account2.Password);
+            account3.EncodePassword();
+            Console.WriteLine("Account 3 Password Hash: {0}", account3.Password);
+        }
+
+        public static void Call(Account account)
+        {
+            if(account.Phone == null)
+            {
+                Console.WriteLine("Account hat keine Telefonnummer");
+            }
+            else
+            {
+                Console.WriteLine("Wähle '{0}'. Peeeeeeeeeep. Geht keiner ran.", account.Phone);
+            }
+        }
+
+        public static void CheckEmail(Account account)
+        {
+            if (account.CheckEmail())
+            {
+                Console.WriteLine("Email '{0}' ist valide", account.EmailAddress);
+            }
+            else
+            {
+                Console.WriteLine("Email '{0}' ist nicht valide", account.EmailAddress);
+            }
+        }
+    }
+}
